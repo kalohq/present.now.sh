691648f6b06777a51c012c180532bf6e2e2b09bd
Tomek Wiszniewski <tomek@lystable.com>
2017-01-11 20:24:58 +0100

    Swatch picker skeleton

--- timer-controls/source/TimerControls.elm
+++ timer-controls/source/TimerControls.elm
@@ -6,7 +6,7 @@ import Html.Polymer exposing (paperSwatchPicker, color, paperButton, ironIcon, i
 import Html.Custom exposing (inlineInput, autofocus, placeholder, valueOnBlur)
 import Html.Events exposing (on, onClick, onInput)
 import Json.Decode exposing (at, string, map)
-import Css exposing ((.), rgb, Snippet, fontFamilies, margin, margin4, marginRight, fontWeight, int, inherit, padding3, px, zero, rem, display, block, textAlign, right, opacity, num, none, property, cursor, pointer, hover)
+import Css exposing ((.), rgb, Snippet, fontFamilies, margin, margin4, marginRight, fontWeight, int, inherit, padding3, px, zero, rem, display, block, textAlign, right, opacity, num, none, property, cursor, pointer, hover, width, height, property, pct, borderRadius)
 import String
 import Dict exposing (Dict)
 import Char
@@ -238,14 +238,17 @@ colorPicker messageWithColor pickedColor =
             in
                 on "color-picker-selected" decode
     in
-        paperSwatchPicker
-            [ class [ SwatchPicker ]
-            , color pickedColor
-            , onColorPickerSelected
-            , colorList """["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#2196f3", "#3f51b5", "#00bcd4", "#03a9f4", "#4caf50", "#009688", "#cddc39", "#8bc34a", "#ffc107", "#ff9800", "#ff5722", "#000000"]"""
-            , columnCount "8"
+        div
+            [ class [ SwatchPicker ] ]
+            [ paperSwatchPicker
+                [ color pickedColor
+                , onColorPickerSelected
+                , colorList """["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#2196f3", "#3f51b5", "#00bcd4", "#03a9f4", "#4caf50", "#009688", "#cddc39", "#8bc34a", "#ffc107", "#ff9800", "#ff5722", "#000000"]"""
+                , columnCount "8"
+                ]
+                [ div [ class [ SwatchPicker_Skeleton ] ] []
+                ]
             ]
-            []
 
 
 colorBreakpoint : ( Int, ColorBreakpoint ) -> List (Html Message)
@@ -281,6 +284,7 @@ colorBreakpoint ( breakpointIndex, breakpoint ) =
 
 type Classes
     = SwatchPicker
+    | SwatchPicker_Skeleton
     | StartButtonContainer
     | StartButtonIcon
     | StartButtonIcon_Disabled
@@ -291,6 +295,13 @@ css : List Snippet
 css =
     [ (.) SwatchPicker
         [ margin (px -16)
+        ]
+    , (.) SwatchPicker_Skeleton
+        [ width (px 24)
+        , height (px 24)
+        , margin (px 16)
+        , borderRadius (pct 50)
+        , property "background" "rgba(0, 0, 0, 0.1)"
         ]
     , (.) StartButtonContainer
         [ margin4 (Css.rem 2) zero zero zero
